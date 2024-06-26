195362be2e77d8530cf811e38252503393b82784
Mightyman Covenant <buikecovenantraphael@gmail.com>
2024-06-26 00:59:12 -0700

    Committing abouttsx and aboutcss

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,11 +1,36 @@
 import React from 'react';
 import { Header } from '../components/Header';
+import '../styles/About.css';
+import { Footer } from '../components/Footer';
 
 export const About = () => {
     return (
         <section>
             <Header/>
-            <p>This is the about page</p>
+            <div className='aboutMainDiv'>
+                <h1 className='aboutFirstText'> Scaling buisness and brand owners </h1>
+                <div className='aboutLoremTextDiv'>
+                    <p className='aboutLoremText'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur, assumenda
+                        perspiciatis. Repellendus suscipit est blanditiis, reprehenderit nihil asperiores ipsum, 
+                        dolore harum rem quam magni beatae voluptatem itaque rerum eaque mollitia?
+                        perspiciatis. Repellendus suscipit est blanditiis, reprehenderit nihil asperiores ipsum, 
+                        dolore harum rem quam magni beatae voluptatem itaque rerum eaque mollitia?
+                        perspiciatis. Repellendus suscipit est blanditiis, reprehenderit nihil asperiores ipsum, 
+                        dolore harum rem quam magni beatae voluptatem itaque rerum eaque mollitia?
+                        perspiciatis. Repellendus suscipit est blanditiis, reprehenderit nihil asperiores ipsum, 
+                        dolore harum rem quam magni beatae voluptatem itaque rerum eaque mollitia?
+                        perspiciatis. Repellendus suscipit est blanditiis, reprehenderit nihil asperiores ipsum, 
+                        dolore harum rem quam magni beatae voluptatem itaque rerum eaque mollitia?
+                        perspiciatis. Repellendus suscipit est blanditiis, reprehenderit nihil asperiores ipsum, 
+                        dolore harum rem quam magni beatae voluptatem itaque rerum eaque mollitia?
+                        perspiciatis. Repellendus suscipit est blanditiis, reprehenderit nihil asperiores ipsum, 
+                        dolore harum rem quam magni beatae voluptatem itaque rerum eaque mollitia?
+                        perspiciatis. Repellendus suscipit est blanditiis, reprehenderit nihil asperiores ipsum, 
+                        dolore harum rem quam magni beatae voluptatem itaque rerum eaque mollitia?</p>
+                </div>
+            </div>
+
+            <Footer/>
         </section>
     )
 }
